998f61e7d1836b0c5d7eb15af3670c8267b77281
yael <lezama.yael.c@gmail.com>
2025-12-09 19:00:50 -0600

    Inicio de Backend

---
+++ src/test/java/mx/uv/Proyecto/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
